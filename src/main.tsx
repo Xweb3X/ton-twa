1fc8a7b98ff744b70fe17aefbd6ae6f6979edb12
justin <justin@163.com>
2024-04-22 16:58:25 +0800

    update config

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://fantasyni.github.io/ton-twa/tonconnect-manifest.json';
+const manifestUrl = 'https://xweb3x.github.io/ton-twa/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
